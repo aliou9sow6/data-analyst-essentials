27e1f3e053d0767677349fe5e5c2953e09bb825c
aliou9sow6 <aliou9sow6@gmail.com>
2025-06-07 01:08:31 +0000

    Adds DataFrame shape and file export functionality
    Enhances the data processing script by adding functionality to display the shape of the DataFrame and export it to both CSV and GZIP CSV formats.
    
    Also includes additional print statements to provide detailed information about the DataFrame, including its structure, data types, and descriptive statistics for better data insight.

--- WQU/Bankruptcy-In-Poland/bankruptcy-in-poland.py
+++ WQU/Bankruptcy-In-Poland/bankruptcy-in-poland.py
@@ -24,3 +24,28 @@ def wrangle(filename) :
 data = wrangle('poland_data/bankruptcy-in-poland.json')
 print("\n'''''''''''''Function output: '''''''''''''\n") # Display the first 5 rows of the DataFrame
 print(data.head())
+print("\n'''''''''''''DataFrame shape: '''''''''''''\n") # Display the shape of the DataFrame
+print(data.shape) # (1000, 2) - 1000 rows and 2 columns
+# Save DataFrame to CSV
+data.to_csv('poland_data/bankruptcy-in-poland.csv', index=False)
+# Save DataFrame to GZIP CSV
+data.to_csv('poland_data/bankruptcy-in-poland.csv.gz', index=False, compression='gzip')
+
+# Read the GZIP CSV file
+data_gzip = pd.read_csv('poland_data/bankruptcy-in-poland.csv.gz', compression='gzip')
+print("\n'''''''''''''GZIP CSV DataFrame shape: '''''''''''''\n") # Display the shape of the DataFrame
+print(data_gzip.shape) # (1000, 2) - 1000 rows and 2 columns
+
+# More information about the dataset
+print("\n'''''''''''''DataFrame info: '''''''''''''\n") # Display the info of the DataFrame
+print(data_gzip.info().__format__) # Display the info of the DataFrame
+
+print("\n'''''''''''''DataFrame columns: '''''''''''''\n") # Display the columns of the DataFrame
+print(data_gzip.columns) # Index(['query', 'data'], dtype='object') - 2 columns: query and data
+
+print("\n'''''''''''''DataFrame dtypes: '''''''''''''\n") # Display the dtypes of the DataFrame
+print(data_gzip.dtypes) # query: object, data: object - both columns are of type object
+
+print("\n'''''''''''''DataFrame describe(include='all'): '''''''''''''\n") # Display the description of the DataFrame
+print(data_gzip.describe(include='all')) # Display the description of the DataFrame including all columns
+
